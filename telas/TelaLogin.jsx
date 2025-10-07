ce8a294ee15a213cba212e35b28c8749d174fe6c
Ana Beatriz <ana.cezar3@etec.sp.gov.br>
2025-10-07 20:36:57 -0300

    botões de navegação

--- telas/TelaLogin.jsx
+++ telas/TelaLogin.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native' 
+import { View, Text, Button } from 'react-native' 
 
-function TelaLogin() {
+function TelaLogin({ navigation}) {
     return (
         <View>
             <Text>Tela de Login</Text>
+
+            <Button
+            title= "Ir para Cadastro"
+            onPress={() => navigation.navigate('Cadastro')}
+            />
+
+            <Button
+            title= "Ir para Descobrir"
+            onPress={() => navigation.navigate('Descobrir')}
+            />
         </View>
     );
 }
